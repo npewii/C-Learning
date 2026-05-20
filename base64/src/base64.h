13e37151c51c99ea715ce205c56120273627b0c8
npewii <nepwii00@outlook.com>
2026-05-20 23:59:52 +0800

    Use pragma once in header

--- base64/src/base64.h
+++ base64/src/base64.h
@@ -1,5 +1,4 @@
-#ifndef BASE64_H
-#define BASE64_H
+#pragma once
 
 #include <stddef.h>
 char* base64_encode(const unsigned char *data, size_t input_length, size_t *output_length);
@@ -7,5 +6,3 @@ unsigned char* base64_decode(const unsigned char *data, size_t input_length, siz
 
 int base64_encode_file(const char *filename);
 int base64_decode_file(const char *filename);
-
-#endif
